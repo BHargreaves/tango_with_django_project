ee934f2d400173a214f9d00f81ac54ccfead9547
Rebekah Hargreaves <2111407H@student.gla.ac.uk>
2018-02-09 12:58:00 +0000

    Fixed error with Chapter 7 -Python indentation error

--- rango/views.py
+++ rango/views.py
@@ -73,13 +73,15 @@ def add_page(request, category_name_slug):
     if request.method == 'POST':
         form = PageForm(request.POST)
         if form.is_valid():
-            page = form.save(commit=False)
-            page.category = category
-            page.views = 0
-            page.save()
-            return show_category(request, category_name_slug)
-        else:
-            print(form.errors)
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+            else:
+                print(form.errors)
 
-        context_dict = {'form': form, 'category': category}
-        return render(request, 'rango/add_page.html', context_dict)
+    context_dict = {'form': form, 'category': category}
+        
+    return render(request, 'rango/add_page.html', context_dict)
